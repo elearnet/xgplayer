717ff2cdbf7a97fcb0d03fdd237e9490839f3815
eadronlin <eadron.lin@gmail.com>
2020-11-06 02:21:48 +0800

    ..

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const Koa = require('koa');
-const Serve = require('koa-static');
-const Cors = require('@koa/cors');
-const Range = require('koa-range');
+const Koa = require('koa')
+const Serve = require('koa-static')
+const Cors = require('@koa/cors')
+const Range = require('koa-range')
 
 const app = new Koa()
 app.use(Range)
 app.use(Cors({
-    origin: '*',
-    allowHeaders: ['range'],
-    allowMethods: ['GET','HEAD','POST','OPTIONS'],
+  origin: '*',
+  allowHeaders: ['range'],
+  allowMethods: ['GET', 'HEAD', 'POST', 'OPTIONS']
 }))
 
 app.use(Serve('.'))
